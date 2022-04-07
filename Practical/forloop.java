7dd4d7abf68434794d77995fe89cb764abbe085e
Surya21101 <96860185+Surya21101@users.noreply.github.com>
2022-04-07 13:36:43 +0530

    Add files via upload

---
+++ Practical/forloop.java
@@ -0,0 +1,12 @@
+class ForExample {
+
+public void show() {
+for(int i=2;i<=10;i=i+2) {
+System.out.println("The valuse is :" +i);
+}
+}
+public static void main(String args[]) {
+ForExample f = new ForExample();
+f.show();
+}
+}
