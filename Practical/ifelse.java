74db7959a6f7c9fe4a6d0226e4f64b65c4c3431c
Surya21101 <96860185+Surya21101@users.noreply.github.com>
2022-04-11 12:52:25 +0530

    Add files via upload

---
+++ Practical/ifelse.java
@@ -0,0 +1,15 @@
+class IfElseExample {
+public void display() {
+int age = 30;
+if(age>=18) {
+System.out.println("You are Eligible");
+}
+else {
+System.out.println("You are not Eligible");
+}
+}
+public static void main(String args[]) {
+IfElseExample b = new  IfElseExample();
+b.display();
+}
+}
