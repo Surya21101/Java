c1490a1c8b996f39dc2931e87146cd1ff047220f
Surya21101 <96860185+Surya21101@users.noreply.github.com>
2022-04-26 13:51:19 +0530

    Add files via upload

--- Assignment/InheritanceActivity.java
+++ Assignment/InheritanceActivity.java
@@ -12,26 +12,34 @@ this.employeeAddress = Address;
 this.employeePhone = phone;
 this.basicSalary = basicSalary;
 }
-public void CalculateSalary() {
-double salary;
-salary = basicSalary + (basicSalary*specialAllowance/100)+(basicSalary*Hra/100);
-System.out.println("Salary :" +salary);
-}
 }
 class Manager extends Employee {
 Manager(long Id, String Name, String Address, long phone, double basicSalary) {
 super(Id, Name, Address, phone, basicSalary);
+
+}
+@Override
+public void calculateTransportAllowance() {
+double transportAllowance = 15*basicSalary/100;
+System.out.println("Transport Allowance for Manager :" +transportAllowance);
 }
 }
 class Trainee extends Employee {
 Trainee(long Id, String Name, String Address, long phone, double basicSalary) {
 super(Id, Name, Address, phone, basicSalary);
 }
+@Override
+public void calculateTransportAllowance() {
+double transportAllowance = 15*basicSalary/100;
+System.out.println("Transport Allowance for Trainee :" +transportAllowance);
+}
 }
 class InheritanceActivity {
 public static void main(String[] args) {
 Manager m = new Manager(126534,"Peter","Chennai,india",237844,65000);
 m.CalculateSalary();
+m.calculateTransportAllowance();
+Trainee t = new Trainee(124543,"Surya","Chennai,india",232000,40000);
+t.calculateTransportAllowance();
 }
 }
-
