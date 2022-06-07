8fcdb50e9f9f69a6f0332c7f20f154d6cc040dba
Surya21101 <96860185+Surya21101@users.noreply.github.com>
2022-06-07 13:41:52 +0530

    Add files via upload

---
+++ Database/Joins.sql
@@ -0,0 +1,19 @@
+use c2110;
+#inner join
+select customer_table.customername,order_table.orderdate from customer_table inner join 
+order_table on customer_table.customerid = order_table.customerid;
+#or
+select customername,orderdate from customer_table inner join 
+order_table on customer_table.customerid = order_table.customerid;
+#or
+select customername,orderdate from customer_table inner join order_table where
+customer_table.customerid = order_table.customerid;
+#leftouterjoin
+select customer_table.customerid,customername,orderdate from customer_table left join
+order_table on customer_table.customerid = order_table.customerid;
+#rightouterjoin
+select customer_table.customerid,customername,orderdate from customer_table right join
+order_table on customer_table.customerid = order_table.customerid;
+#foreign key
+alter table order_table add foreign key(customerid) references customer_table(customerid); 
+
