c7165ce545cf9b39f32349e60a5f0151a5ce75c0
Surya21101 <96860185+Surya21101@users.noreply.github.com>
2022-06-08 13:02:44 +0530

    Update Joins.sql

--- Database/Joins.sql
+++ Database/Joins.sql
@@ -35,3 +35,14 @@ select * from emp where emp_id in (select emp_id from emp where salary > 1200);
 select * from emp where salary = (select max(salary) from emp);
 select * from emp where addr not in (select addr from emp where addr = 'mumbai');
 select * from emp where dept !=(select dept from emp where dept = 'operation');
+select * from emp where salary >(select salary from emp where ename = 'john');
+#Any
+select customerid,customername from customer_table 
+where customerid > any (select customerid from order_table);
+#All
+select customerid,customername from customer_table 
+where customerid > all (select customerid from order_table);
+#not exists
+select customername,customerphone from customer_table 
+where not exists 
+(select * from order_table where customer_table.customerid = ordertable.customerid);
