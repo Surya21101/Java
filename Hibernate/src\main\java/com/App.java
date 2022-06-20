1e4469e231deb7efb3023d293659d49cb81222d6
Surya21101 <96860185+Surya21101@users.noreply.github.com>
2022-06-20 13:56:46 +0530

    Create App.java

---
+++ Hibernate/src\main\java/com/App.java
@@ -0,0 +1,23 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import com.Entity.Employee;
+public class App {
+public static void main( String[] args ) {
+SessionFactory s = new Configuration().configure().buildSessionFactory();
+Session ses = s.openSession();
+Transaction t = ses.beginTransaction();
+Employee e = new Employee();
+e.setEmpId(101);
+e.setEmpFirstName("Mamta");
+e.setEmpLastName("Boga");
+e.setEmpEmail("mamta@gmail.com");
+e.setEmpPhone(834822288);
+e.setEmpAddr("Mumbai");
+e.setEmpSalary(12300.98);	
+ses.save(e);
+t.commit();
+ses.close();
+}
+}
