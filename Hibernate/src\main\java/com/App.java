0c57fb201daa402c6db73358e3c1542168d7a620
Surya21101 <96860185+Surya21101@users.noreply.github.com>
2022-06-20 16:13:50 +0530

    Update App.java

--- Hibernate/src\main\java/com/App.java
+++ Hibernate/src\main\java/com/App.java
@@ -5,10 +5,17 @@ import org.hibernate.cfg.Configuration;
 import com.Entity.Employee;
 public class App {
 public static void main( String[] args ) {
+//Configuration -activates hibernate framework
+//configure -read both cfg files and mapping details
+//buildSessionFactory -from cfg object it takes jdbc information and create a jdbc connection
 SessionFactory s = new Configuration().configure().buildSessionFactory();
+//opens an connection with db and session object perform CRUD
 Session ses = s.openSession();
+//operation performed in db,save permanent
 Transaction t = ses.beginTransaction();
+//create object of persistent class
 Employee e = new Employee();
+//use object to enter data in table using setter method
 e.setEmpId(101);
 e.setEmpFirstName("Mamta");
 e.setEmpLastName("Boga");
@@ -16,8 +23,11 @@ e.setEmpEmail("mamta@gmail.com");
 e.setEmpPhone(834822288);
 e.setEmpAddr("Mumbai");
 e.setEmpSalary(12300.98);	
+//session object is used to save the persistent object
 ses.save(e);
+//transaction object to commit changes
 t.commit();
+//closing session
 ses.close();
 }
 }
